2fb87a4ffb02d490a53bbe1ff3b5ea67408a5fac
Leonid Rodak <LeonidRodak@users.noreply.github.com>
2026-05-25 17:29:17 +1000

    Код под PEP8

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,7 +3,9 @@ from posts.models import Post, Group, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(slug_field="username", read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field="username", read_only=True
+    )
 
     class Meta:
         model = Post
@@ -17,7 +19,9 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(slug_field="username", read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field="username", read_only=True
+    )
 
     class Meta:
         model = Comment
